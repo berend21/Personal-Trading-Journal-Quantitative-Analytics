f31ccfbfa927ba887170c33a9504797744edb282
Berend <52155322+berend21@users.noreply.github.com>
2026-08-21 17:09:25 +0200

    Update extensions.py
    Added limiter

--- extensions.py
+++ extensions.py
@@ -1,3 +1,12 @@
 from flask import Flask
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
+
+limiter = Limiter(
+    key_func=get_remote_address,
+    default_limits=[]
+)
+
+limiter.init_app(app)
