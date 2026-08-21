dda0ab2765591168c10a5f3a5f0aa452647168c5
Berend <52155322+berend21@users.noreply.github.com>
2026-08-21 17:08:43 +0200

    Update login.py
    Cleaned up code a bit and added limiter

--- login.py
+++ login.py
@@ -1,14 +1,10 @@
-import re
-
 from functools import wraps
-
 from flask import render_template, flash, session, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 from werkzeug.security import check_password_hash
-from extensions import app
-
+from extensions import app, limiter
 from database import get_db
 
 class LoginForm(FlaskForm):
@@ -17,21 +13,13 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 @app.route('/login', methods=['GET', 'POST'])
+@limiter.limit("5 per minute")
 def login():
     form = LoginForm()
     
     if form.validate_on_submit(): 
-        email = form.email.data
+        email = form.email.data.strip().lower()
         password = form.password.data
-
-        email_regex = r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'
-        if not re.match(email_regex, email):
-            flash('Please enter a valid email address.', 'error')
-            return render_template('login.html', form=form)  
-        
-        if len(password) < 7:
-            flash('Password must be at least 7 characters long.', 'error')
-            return render_template('login.html', form=form)
 
         conn = get_db()
         user = conn.execute(
@@ -39,17 +27,16 @@ def login():
             (email,)
         ).fetchone()
 
-        
         if user and check_password_hash(user['password'], password):
-            session['logged_in'] = True
+            session.clear()
+            session['user_id'] = user['id']
             session['username'] = email
-            print("Redirecting to:", url_for('index', _external=True))
-            return redirect(url_for('index', _external=True))
+            session.permanent = True
+            return redirect(url_for('index'))
+
         else:
             flash('Invalid credentials', 'error')
             return render_template('login.html', form=form)  
-    
-
     return render_template('login.html', form=form)
 
 @app.route('/logout')
@@ -60,7 +47,7 @@ def logout():
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if not session.get('logged_in'):
+        if not session.get('user_id'):
             return redirect(url_for('login'))
         return f(*args, **kwargs)
     return decorated_function
