531465152545cf526569c21bd642ba1d0a19f288
Berend <52155322+berend21@users.noreply.github.com>
2026-08-28 12:02:52 +0200

    Update config.py
    Updating secret key

--- config.py
+++ config.py
@@ -1,15 +1,23 @@
 from extensions import app
-import configparser
+import os
+
+SECRET_KEY = os.environ.get("FLASK_SECRET_KEY")
+if not SECRET_KEY:
+    raise RuntimeError(
+        "FLASK_SECRET_KEY is not set. "
+        "Set it before starting the app."
+    )
+
+app.config["SECRET_KEY"] = SECRET_KEY
+
 
 app.config['SESSION_COOKIE_SAMESITE'] = "Strict"
 app.config['SESSION_COOKIE_SECURE'] = False
 app.config['SESSION_COOKIE_HTTPONLY'] = True 
+
 
 app.config['PERMANENT_SESSION_LIFETIME'] = 86400
 app.config['WTF_CSRF_TIME_LIMIT'] = 86400
 app.config['UPLOAD_FOLDER']= 'static/uploads'
 app.config["MAX_CONTENT_LENGTH"] = 512*1024*1024
 
-config = configparser.ConfigParser()
-config.read('config.ini')
-app.secret_key = config['flask']['secret_key']
