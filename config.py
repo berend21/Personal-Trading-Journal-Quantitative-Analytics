6be68447e8c3639f0d7de4c79f2fc8f41be2adfd
Berend <52155322+berend21@users.noreply.github.com>
2026-08-28 17:19:24 +0000

    Update config.py
    Lower upload size.

--- config.py
+++ config.py
@@ -19,5 +19,5 @@ app.config['SESSION_COOKIE_HTTPONLY'] = True
 app.config['PERMANENT_SESSION_LIFETIME'] = 86400
 app.config['WTF_CSRF_TIME_LIMIT'] = 86400
 app.config['UPLOAD_FOLDER']= 'static/uploads'
-app.config["MAX_CONTENT_LENGTH"] = 512*1024*1024
+app.config["MAX_CONTENT_LENGTH"] = 64*1024*1024
 
