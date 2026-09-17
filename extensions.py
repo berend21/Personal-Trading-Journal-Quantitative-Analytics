b86f6c59da445f61d661d99899f5cde50773a96a
Berend <52155322+berend21@users.noreply.github.com>
2026-09-17 19:49:24 +0200

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask_wtf.csrf import CSRFProtect
+
+from symbol_icons import get_symbol_icon
+
 
 app = Flask(__name__)
+
+app.jinja_env.globals["symbol_icon_data"] = get_symbol_icon
+
+csrf = CSRFProtect(app)
 
 limiter = Limiter(
     key_func=get_remote_address,
